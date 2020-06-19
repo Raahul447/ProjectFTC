8722782ec7c6677760f80b8fee03f929eab77772
Raahul447 <47570178+Raahul447@users.noreply.github.com>
2020-06-19 18:38:39 -0400

    Pushed menu and level 1 changes
    new level 1 fixes and menu fixes

---
+++ FTC/Assets/Scripts/StopMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StopMove : MonoBehaviour
+{
+    public TutorialMovement TM;
+    public string levelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void stopmove()
+    {
+        TM.enabled = false;
+    }
+
+    public void startmove()
+    {
+        TM.enabled = true;
+    }
+
+    public void levelchange()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
