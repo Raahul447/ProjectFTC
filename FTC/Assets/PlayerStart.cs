b6b7e2e8ea0fdc674111c1a6d356b1f2a09eeb09
rohit-lad <rohit-lad@users.noreply.github.com>
2020-07-20 13:18:00 -0700

    Player Start Update
    Changes to script land the player on the start cube

--- FTC/Assets/PlayerStart.cs
+++ FTC/Assets/PlayerStart.cs
@@ -3,29 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class PlayerSpawn : MonoBehaviour
+public class PlayerStart : MonoBehaviour
 {
     private GameObject player;
-
+    public float Y = 2.444f;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
-
     private void OnTriggerEnter(Collider other)
     {
         if (player)
         {
-            player.GetComponent<Movement>().enabled = false;
-            StartCoroutine(movePlayer());
+            player.GetComponent<Movement>().enabled = true;
+            player.GetComponent<Rigidbody>().isKinematic = true;
+            player.transform.DOMoveY(Y, 0.5f);
         }
-    }
-
-    IEnumerator movePlayer()
-    {
-        yield return new WaitForSeconds(1f);
-        player.GetComponent<Rigidbody>().isKinematic = false;
     }
 }
