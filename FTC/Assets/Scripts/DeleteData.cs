4c778cde2abb25f77598043dc7a651d73b220b11
Raahul447 <47570178+Raahul447@users.noreply.github.com>
2020-07-27 17:00:21 -0400

    Some changes
    Changes pushed

---
+++ FTC/Assets/Scripts/DeleteData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DeleteData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeletePrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
