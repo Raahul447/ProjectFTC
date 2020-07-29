7ce99dade052a96c86ef15dd18dcf07d4093ea22
rohit-lad <rohit-lad@users.noreply.github.com>
2020-07-29 12:10:03 -0700

    Level Update
    New script to lock the aspect ratio of the camera
    Added the new script to camera in all scenes

---
+++ FTC/Assets/LockAspectRatio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockAspectRatio : MonoBehaviour
+{
+    
+    const int resolutionX = 9;
+    const int resolutionY = 16;
+
+    void Start()
+    {
+        float screenRatio = Screen.width * 1f / Screen.height;
+        float bestRatio = resolutionX * 1f / resolutionY;
+        if (screenRatio <= bestRatio)
+        {
+            GetComponent<Camera>().rect = new Rect(0, (1f - screenRatio / bestRatio) / 2f, 1, screenRatio / bestRatio);
+        }
+        else if (screenRatio > bestRatio)
+        {
+            GetComponent<Camera>().rect = new Rect((1f - bestRatio / screenRatio) / 2f, 0, bestRatio / screenRatio, 1);
+        }
+    }
+}
