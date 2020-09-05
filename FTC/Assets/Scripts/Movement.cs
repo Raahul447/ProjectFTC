87446a8f51e4dc4d2d0467bc4d1d1ea762877665
rohit-lad <rohit-lad@users.noreply.github.com>
2020-09-05 03:18:22 -0400

    Movement script update
    Changed the movement script to disable movement in any direction without a platform by using Raycast

--- FTC/Assets/Scripts/Movement.cs
+++ FTC/Assets/Scripts/Movement.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine.UI;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class Movement : MonoBehaviour
 {
@@ -22,15 +24,50 @@ public class Movement : MonoBehaviour
     //public ScoreCal sc;
     public int _Moves = 0;
 
+    public bool canMoveLeft = true;
+    public bool canMoveRight = true;
+    public bool canMoveForward = true;
+    public bool canMoveBackward = true;
+
+    public float raycastHitDistance = 2;
+
+    //public List<Transform> pathPoints = new List<Transform>();
+    //[SerializeField]
+    //private Transform playerPos;
+
+    //public void OnDrawGizmos()
+    //{
+    //    if (pathPoints == null || pathPoints.Count < 2)
+    //    {
+    //        return;
+    //    }
+    //    for (var i = 1; i < pathPoints.Count; i++)
+    //    {
+    //        Gizmos.DrawLine(pathPoints[i - 1].position, pathPoints[i].position);
+    //    }
+    //}
     void Start()
     {
+        //playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        //if (pathPoints == null)
+        //{
+        //    Debug.LogError("Movement Path cannot be null, I must have a path to follow.", gameObject);
+        //    return;
+        //}
         newRotation = oldRotation = transform.rotation;
     }
 
     void Update()
     {
         // get input from user and update state if need be
+        //if (pathPoints.Equals(playerPos))
+        //{
         UpdateRotationState();
+        //}
+        //else
+        //{
+        //    Debug.Log("Can't Move");
+        //}
         // if the lerp time is less than one, lerp to the new rotation
         if (lerpTime < 1f)
         {
@@ -42,40 +79,104 @@ public class Movement : MonoBehaviour
     }
     void UpdateRotationState()
     {
+        var forward = Vector3.forward;
+        var right = Vector3.right;
+
+        canMoveBackward = true;
+        canMoveForward = true;
+        canMoveLeft = true;
+        canMoveRight = true;
+
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, forward * raycastHitDistance, Color.red);
+        Debug.DrawRay(transform.position, -forward * raycastHitDistance, Color.green);
+        Debug.DrawRay(transform.position, -right * raycastHitDistance, Color.white);
+        Debug.DrawRay(transform.position, right * raycastHitDistance, Color.black);
+
+        if (Physics.Raycast(transform.position, -right, out hit, raycastHitDistance))
+        {
+            Debug.Log("Left Hit");
+            if (hit.collider.gameObject.tag == "Barrier")
+            {
+                canMoveLeft = false;
+            }
+        }
+
+        if (Physics.Raycast(transform.position, -forward, out hit, raycastHitDistance))
+        {
+            Debug.Log("Back Hit");
+            if (hit.collider.gameObject.tag == "Barrier")
+            {
+                canMoveBackward = false;
+            }
+        }
+
+        if (Physics.Raycast(transform.position, forward, out hit, raycastHitDistance))
+        {
+            Debug.Log("Front Hit");
+            if (hit.collider.gameObject.tag == "Barrier")
+            {
+                canMoveForward = false;
+            }
+        }
+
+        if (Physics.Raycast(transform.position, right, out hit, raycastHitDistance))
+        {
+            Debug.Log("Right Hit");
+            if (hit.collider.gameObject.tag == "Barrier")
+            {
+                canMoveRight = false;
+            }
+        }
+
         // Get the rotation, if any
         float x = 0f, z = 0f;
 
         //Left
-        if (Input.GetKeyDown(KeyCode.A) || SwipeManager.IsSwipingUpLeft())
+        if (canMoveLeft)
         {
-            z = 90f;
-            transform.position += Vector3.left * 2;
-            _Moves += 1;
+            if (Input.GetKeyDown(KeyCode.A) || SwipeManager.IsSwipingUpLeft())
+            {
+                z = 90f;
+                transform.position += Vector3.left * 2;
+                _Moves += 1;
+            }
         }
 
         //Down
-        if (Input.GetKeyDown(KeyCode.S) || SwipeManager.IsSwipingDownLeft() || SwipeManager.IsSwipingDown() | SwipeManager.IsSwipingLeft())
+        if (canMoveBackward)
         {
-            x = -90f;
-            transform.position += Vector3.back * 2;
-            _Moves += 1;
+            if (Input.GetKeyDown(KeyCode.S) || SwipeManager.IsSwipingDownLeft() || SwipeManager.IsSwipingDown() | SwipeManager.IsSwipingLeft())
+            {
+                x = -90f;
+                transform.position += Vector3.back * 2;
+                _Moves += 1;
+            }
         }
 
         //Up
-        if (Input.GetKeyDown(KeyCode.W) || SwipeManager.IsSwipingUpRight() || SwipeManager.IsSwipingUp() || SwipeManager.IsSwipingRight())
+        if (canMoveForward)
         {
-            x = 90f;
-            transform.position += Vector3.forward * 2;
-            _Moves += 1;
+            if (Input.GetKeyDown(KeyCode.W) || SwipeManager.IsSwipingUpRight() || SwipeManager.IsSwipingUp() || SwipeManager.IsSwipingRight())
+            {
+                x = 90f;
+                transform.position += Vector3.forward * 2;
+                _Moves += 1;
+            }
         }
 
         //Right
-        if (Input.GetKeyDown(KeyCode.D) ||SwipeManager.IsSwipingDownRight())
+        if (canMoveRight)
         {
-            z = -90f;
-            transform.position += Vector3.right * 2;
-            _Moves += 1;
+            if (Input.GetKeyDown(KeyCode.D) || SwipeManager.IsSwipingDownRight())
+            {
+                z = -90f;
+                transform.position += Vector3.right * 2;
+                _Moves += 1;
+            }
         }
+
 
         // if rotation is nonzero, apply it
         if (x != 0f || z != 0f)
@@ -84,5 +185,7 @@ public class Movement : MonoBehaviour
             oldRotation = transform.rotation;
             lerpTime = 0f;
         }
+    
     }
-}
+} 
+
