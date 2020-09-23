c0379e29732140b552ef9e29347e87f204ae29e3
rohit-lad <rohit-lad@users.noreply.github.com>
2020-09-23 18:10:52 -0400

    Level_1 Update
    New finish tile diamond

--- FTC/Assets/Scripts/Diamond.cs
+++ FTC/Assets/Scripts/Diamond.cs
@@ -9,6 +9,8 @@ public class Diamond : MonoBehaviour
     public float speed;
     public GameObject diamond;
 
+    public bool isPlayerUnder = false;
+
     public float yPos;
     public float time;
 
@@ -25,21 +27,41 @@ public class Diamond : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, Time.deltaTime * speed, 0));
-    }
+        var down = Vector3.down;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, down * 4, Color.white);
+
+        if (Physics.Raycast(transform.position, down, out hit, 4))
+        {
+            //Debug.Log("Left Hit");
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                isPlayerUnder = true;
+            }
+        }
+
+        if(isPlayerUnder)
         {
             StartCoroutine(DiamondEnd());
         }
+
+        transform.Rotate(new Vector3(0, Time.deltaTime * speed, 0));
     }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        StartCoroutine(DiamondEnd());
+    //    }
+    //}
 
     IEnumerator DiamondEnd()
     {
         diamond.GetComponent<Renderer>().material = g_Material;
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.7f);
         diamond.transform.DOScale(new Vector3(0, 0, 0), 1);
         yield return new WaitForSeconds(0.5f);
         diamond.SetActive(false);
