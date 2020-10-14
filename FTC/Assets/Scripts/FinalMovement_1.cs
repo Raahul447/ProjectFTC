510c467aab18d5519c3929a36bdef69c7a1453a5
Raahul447 <47570178+Raahul447@users.noreply.github.com>
2020-10-14 16:48:46 -0400

    PUshing new changes
    Level 21 and new scripts

---
+++ FTC/Assets/Scripts/FinalMovement_1.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using MilkShake;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class FinalMovement_1 : MonoBehaviour
+{
+    //Movement 
+    public float lerpSpeed = 10f;
+    float lerpTime = 1f;
+    Quaternion newRotation;
+    Quaternion oldRotation;
+
+    //Swipe Controls
+    private float swipeResistanceX = 70.0f;
+    private float swipeResistanceY = 100.0f;
+    Vector2 touchPos;
+
+    public int count = 0;
+
+    public Animator welcomeAnim;
+    public bool isFront = false;
+    public bool isBack = false;
+
+    //[Header("Shake")]
+    //public Shaker myShaker;
+    //public ShakePreset CamShake;
+
+    void Start()
+    {
+        newRotation = oldRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+
+        //Shakes();
+
+        // get input from user and update state if need be
+        UpdateRotationState();
+        // if the lerp time is less than one, lerp to the new rotation
+        if (lerpTime < 1f)
+        {
+            lerpTime += Time.deltaTime * lerpSpeed;
+            transform.rotation = Quaternion.Lerp(oldRotation, newRotation, lerpTime);
+        }
+    }
+
+    void UpdateRotationState()
+    {
+        // Get the rotation, if any
+        float x = 0f, z = 0f;
+        //Up
+        if ((Input.GetKeyDown(KeyCode.W) || SwipeManager.IsSwipingUpRight()) && isFront)
+        {
+            //print("Forward");
+            x = 90f;
+            transform.position += Vector3.forward * 2;
+            count += 1;
+        }
+        //Down
+        else if ((Input.GetKeyDown(KeyCode.S) || SwipeManager.IsSwipingDownLeft()) && isBack)
+        {
+            //print("Back");
+            x = -90f;
+            transform.position += Vector3.back * 2;
+        }
+
+        // if rotation is nonzero, apply it
+        if (x != 0f || z != 0f)
+        {
+            newRotation = Quaternion.Euler(x, 0f, z) * newRotation;
+            oldRotation = transform.rotation;
+            lerpTime = 0f;
+        }
+
+        //myShaker.Shake(CamShake);
+    }
+
+    //public void Shakes()
+    //{
+
+    //    if ((Input.GetKeyDown(KeyCode.W) || SwipeManager.IsSwipingUpRight()) && !isFront)
+    //    {
+    //        myShaker.Shake(CamShake);
+
+    //    }
+    //    else if ((Input.GetKeyDown(KeyCode.S) || SwipeManager.IsSwipingDownLeft()) && !isBack)
+    //    {
+    //        myShaker.Shake(CamShake);
+    //        Debug.Log("Shake");
+    //    }
+    //}
+}
--- FTC/Assets/Scripts/SplashScreen.cs
+++ FTC/Assets/Scripts/SplashScreen.cs
@@ -7,9 +7,79 @@ using UnityEngine;
 
 public class SplashScreen : MonoBehaviour
 {
+
+    [Header("Values")]
+    public float timer;
+
+    [Header("Part 1")]
+    public GameObject obj1;
+    public GameObject obj2;
+    public GameObject obj3;
+    public GameObject obj4;
+
+    [Header("Part 1.5")]
+    public GameObject obj5;
+
+    [Header("Part 2")]
+    public GameObject obj6;
+    public GameObject obj7;
+    public GameObject obj8;
+    public GameObject obj9;
+
+    [Header("Audio")]
+    public AudioSource sound;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(Splash());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Splash()
+    {
+        obj1.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        obj2.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        obj3.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        obj4.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        obj5.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        obj6.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        obj7.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        obj8.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        obj9.SetActive(true);
+        yield return new WaitForSeconds(timer);
+        obj1.SetActive(false);
+        yield return new WaitForSeconds(timer);
+        obj2.SetActive(false);
+        yield return new WaitForSeconds(timer);
+        obj3.SetActive(false);
+        yield return new WaitForSeconds(timer);
+        obj4.SetActive(false);
+        yield return new WaitForSeconds(timer);
+        obj6.SetActive(false);
+        yield return new WaitForSeconds(timer);
+        obj7.SetActive(false);
+        yield return new WaitForSeconds(timer);
+        obj8.SetActive(false);
+        yield return new WaitForSeconds(timer);
+        obj9.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        obj5.transform.DOLocalMoveX(500, 0.5f);
+        yield return new WaitForSeconds(0.4f);
         if (PlayerPrefs.GetInt("Tutorial") == 0)
         {
             //First launch
@@ -21,11 +91,6 @@ public class SplashScreen : MonoBehaviour
             //Load scene_02 if its not the first launch
             SceneManager.LoadScene("Main_Menu_V2");
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
